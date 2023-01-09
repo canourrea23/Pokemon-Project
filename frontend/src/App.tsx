6e1bd30f4732096d13e44c15245fa52ad9c19752
Carolina Urrea <curestrepo@gmail.com>
2023-01-08 19:54:38 -0800

    Creating pokemon objects in components/row and trying to call it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,33 +2,44 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 
 function PokemonRow(props) {
-  console.log(props);
   return (
     <div>
-      {props.name} <button> X</button>
+      {props.name}
+      <button onClick={() => props.handleDelete(props.id)}> X</button>
     </div>
   );
 }
 
 function PokemonList(props) {
+  console.log(props);
   return (
     <div>
       {props.list.map((pokemon) => (
-        <PokemonRow name={pokemon.name} id={pokemon.id} />
+        <PokemonRow
+          key={pokemon.id}
+          name={pokemon.name}
+          id={pokemon.id}
+          handleDelete={props.handleDelete}
+        />
       ))}
     </div>
   );
 }
 
 function App() {
-  const [count, setCount] = useState(0);
-  const API = [
+  const [list, setList] = useState([
     { name: "pikachu", id: 25 },
     { name: "charmander", id: 4 },
-  ];
+  ]);
+  console.log(list);
+  function handleDelete(id) {
+    const newList = list.filter((pokemon) => pokemon.id != id);
+    setList(newList);
+  }
   return (
     <div className="App">
-      <PokemonList list={API} />
+      {/* <PokemonList list={list} handleDelete={handleDelete} /> */}
+      {PokemonList({ list, handleDelete })}
     </div>
   );
 }
